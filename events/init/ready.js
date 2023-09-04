736ee89af360b7999502a97818cced6c13699a43
k4rm1 <134770238+k4rm1@users.noreply.github.com>
2023-09-04 15:08:12 +0200

    Add files via upload

--- events/init/ready.js
+++ events/init/ready.js
@@ -15,64 +15,6 @@ module.exports = {
      */
     run: async (client) => {
        
-        request(`http://localhost:3000/getbuyer/${client.user.id}`, async (err, res, body) => {
-            let data = JSON.parse(body).body
-
-        request.get(`http://localhost:3000/getbot/${client.user.id}`, (err, res, body) => {
-            if (err) return client.destroy()
-            if (body === "Bot does not exist") {
-                client.destroy()
-                exec(`pm2 stop ${client.user.id} && pm2 save`, (err, stdout, stderr) => {})
-                console.log(`Violation has been detected, fraudulent bot has been destroyed.`)
-                return
-            }
-            const bot = JSON.parse(body).body
-            if (bot.time - Date.now() <= 0) {
-            client.users.cache.get(data)?.send(`Ce bot est arrivé à expiration, pour le renouveler mentionnez un administrateur dans votre channel privé`).then(async () => {
-                client.destroy()
-               exec(`pm2 stop ${client.user.id} && pm2 save`, (err, stdout, stderr) => {})
-                
-                console.log(`Expired bot has been destroyed.`)
-            })
-            }
-        })
-        
-
-
-        request.get(`http://localhost:3000/gestion/version`, (err, res, body) => {
-            if (err) return;
-            if (body !== client.version) {
-                console.log(`Mise à jour disponible: ${body}`)
-            } else {
-                console.log(`Bot mis à jour`)
-            }
-        })
-
-        setInterval(() => {
-           
-        request.get(`http://localhost:3000/getbot/${client.user.id}`, (err, res, body) => {
-            if (err) return client.destroy()
-            if (body === "Bot does not exist") {
-                client.destroy()
-                exec(`pm2 stop ${client.user.id} && pm2 save`, (err, stdout, stderr) => {})
-                console.log(`Violation has been detected, fraudulent bot has been destroyed.`)
-                return
-            }
-            const bot = JSON.parse(body).body
-            if (bot.time - Date.now() <= 0) {
-            client.users.cache.get(data)?.send(`Ce bot est arrivé à expiration, pour le renouveler mentionnez un administrateur dans votre channel privé`).then(async () => {
-                client.destroy()
-                exec(`pm2 stop ${client.user.id} && pm2 save`, (err, stdout, stderr) => {})
-                console.log(`Expired bot has been destroyed.`)
-            })
-            }
-        })
-
-        
-        }, ms("1m"))
-
-        })
-
         setInterval(() => {
            
         if (client.db.get(`isActivityOn`) === "remove") {
